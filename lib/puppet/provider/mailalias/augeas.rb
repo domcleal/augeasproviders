voxpupuli/puppet-augeasproviders

Make providers default to augeas when the augeas feature is present

lib/puppet/provider/mailalias/augeas.rb
@@ -12,6 +12,7 @@ Puppet::Type.type(:mailalias).provide(:augeas) do
 
   confine :feature => :augeas
   confine :exists => "/etc/aliases"
+  defaultfor :features => :augeas
 
   def self.file(resource = nil)
     file = "/etc/aliases"

domcleal/augeasproviders

lib/puppet/provider/mailalias/augeas.rb
@@ -12,6 +12,7 @@ Puppet::Type.type(:mailalias).provide(:augeas) do
 
   confine :feature => :augeas
   confine :exists => "/etc/aliases"
+  defaultfor :feature => :augeas
 
   def self.file(resource = nil)
     file = "/etc/aliases"